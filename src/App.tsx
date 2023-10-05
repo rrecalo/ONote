ee34fa500093d2a8bbdfe68bb0aa8987ee267b40
Rrazmik <robertrecalo@icloud.com>
2023-10-05 18:05:50 -0400

    Add login/logout feature skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React , {useEffect} from 'react';
 import './App.css';
+import { useAuth0 } from '@auth0/auth0-react';
+import LoginButton from './LoginButton';
 
 function App() {
+
+  const { user, isAuthenticated, isLoading, logout } = useAuth0();
+
   return (
     <div className="App">
-      
+      {
+      !isAuthenticated ? <LoginButton /> : 
+      <div>
+      <div className='text-2xl font-light'>Looks like you're logged in!</div>
+      <div onClick={() => 
+        logout(
+          { 
+            logoutParams: {returnTo: window.location.origin}
+          }
+          )
+        }>Click here to logout!</div>
+      </div>
+      }
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
+  <Auth0Provider
+    domain="notes158694.us.auth0.com"
+    clientId="JTgCuc1CydAfp31fjwR0Lr9WEX6WWdGV"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}>
     <App />
+  </Auth0Provider>
 );
 
