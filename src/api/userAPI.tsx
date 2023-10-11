ecd100a98aa3373ef9e70cb7830ceda0ab6532a6
Rrazmik <robertrecalo@icloud.com>
2023-10-11 14:19:12 -0400

    New API method to update a note in the DB

--- src/api/userAPI.tsx
+++ src/api/userAPI.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import {Note} from '../types/Note';
 
 const BASE_URL = "http://localhost:3001";
 //http://localhost:3001/getUserNotes
@@ -14,4 +15,9 @@ export async function createNote(email : string | undefined, text : string | und
     let endpoint = "/createNote";
     return axios.post(BASE_URL + endpoint, {email : email, text: text}).then((response) =>{return(response)});
 
+}
+
+export async function updateNote(note : Note){
+    let endpoint = "/updateNote";
+    return axios.put(BASE_URL + endpoint, {title : note.title, _id: note._id, text: note.text}).then((response) => {return(response)});
 }
