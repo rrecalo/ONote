2cc03836d0a49707b809ca0aef512f69e7191fb1
Robert <robertrecalo@icloud.com>
2024-01-30 21:45:18 -0500

    Responsiveness and position fixes

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -32,11 +32,8 @@ function Landing(){
     return (
     <div className='w-full h-full min-h-[100vh] m-0 p-0 bg-stone-50'>
         <div className="w-full sm:w-3/4 mx-auto">
-            <div id="nav" className="w-full h-10">
-                
-            </div>
-            <div className="w-full text-center">
-                <motion.div className='w-1/2 mx-auto flex justify-between'
+            <div className="w-full text-center mt-10">
+                <motion.div className='w-5/6 sm:w-1/2 mx-auto flex justify-between'
                 initial={{opacity:0, y:-3}} animate={{opacity:1, y:0}} transition={{delay:1, duration:0.5}}>
                     <motion.a target="_blank" href="https://github.com/rrecalo/ONote"
                     className='text-md p-3 rounded-lg block w-fit text-stone-700'
@@ -55,7 +52,7 @@ function Landing(){
                         {/*<MdOutlineArrowForward className='inline-block'/>*/}
                     </motion.a>
                 </motion.div>
-                <motion.div className='text-6xl align-center text-stone-800 mt-10 mb-2'
+                <motion.div className='text-6xl align-center text-stone-800 mt-20 sm:mt-10 mb-2'
                 initial={{opacity:0}} animate={{opacity:1}} transition={{duration:0.25}}        
                 >
                     <BsTextParagraph className='text-6xl inline-block'/>
@@ -63,12 +60,12 @@ function Landing(){
                         ONote
                     </div>
                 </motion.div>
-                <motion.span className='text-2xl font-light text-stone-800 text-center w-full'
+                <motion.span className='block w-3/4 mx-auto text-lg sm:text-2xl font-light text-stone-800 text-center w-full'
                 initial={{opacity:0}} animate={{opacity:1}} transition={{duration:0.4}}>
                     A fuss-free cloud solution for your note-taking woes
                 </motion.span>
                 
-                <motion.div className="cursor-pointer w-fit h-fit text-md p-2.5 px-4 mt-5 mx-auto
+                <motion.div className="cursor-pointer text-lg sm:text-xl w-1/2 sm:w-fit sm:h-fit text-md p-2.5 px-4 mt-10 mx-auto
                 bg-stone-900 text-stone-50 rounded-md" variants={button} initial="initial" animate="animate" whileHover="whileHover"
                 onClick={handleCallToActionClick}>  
                     Get Started
